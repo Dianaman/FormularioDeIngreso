4afce436df1dc7d1ece011db2d6211929ab5bc81
Diana <diana.man.91@gmail.com>
2016-03-19 14:10:23 -0300

    reorganizacion

--- UTN/3er Cuatrimestre/Programacion3/T1_FormularioIngreso/index.php
+++ UTN/3er Cuatrimestre/Programacion3/T1_FormularioIngreso/index.php
@@ -33,51 +33,54 @@
         </fieldset>
         
         <fieldset>
-          <legend><span class="number">2</span>Your profile</legend>
+          <legend><span class="number">2</span>Your job info</legend>
           <label for="bio">Biography:</label>
           <textarea id="bio" name="user_bio"></textarea>
+          
+          <label for="job">Job Role:</label>
+          <select id="job" name="user_job">
+            <optgroup label="Web">
+              <option value="frontend_developer">Front-End Developer</option>
+              <option value="php_developor">PHP Developer</option>
+              <option value="python_developer">Python Developer</option>
+              <option value="rails_developer"> Rails Developer</option>
+              <option value="web_designer">Web Designer</option>
+              <option value="WordPress_developer">WordPress Developer</option>
+            </optgroup>
+            <optgroup label="Mobile">
+              <option value="Android_developer">Androild Developer</option>
+              <option value="iOS_developer">iOS Developer</option>
+              <option value="mobile_designer">Mobile Designer</option>
+            </optgroup>
+            <optgroup label="Business">
+              <option value="business_owner">Business Owner</option>
+              <option value="freelancer">Freelancer</option>
+            </optgroup>
+            <optgroup label="Other">
+              <option value="secretary">Secretary</option>
+              <option value="maintenance">Maintenance</option>
+            </optgroup>
+          </select>
+          
+          <label>Interests:</label>
+          <input type="checkbox" id="development" value="interest_development" name="user_interest"><label class="light" for="development">Development</label><br>
+            <input type="checkbox" id="design" value="interest_design" name="user_interest"><label class="light" for="design">Design</label><br>
+          <input type="checkbox" id="business" value="interest_business" name="user_interest"><label class="light" for="business">Business</label><br>
+          <br>
+          
+          <label for="url">URL:</label>
+          <input type="url" id="site" name="user_site">
 
+        </fieldset>
+        <fieldset>
+          <legend><span class="number">3</span>Extras</legend>
           <label>Favorite number:</label>
           <input type="number" id="fav_num"name="user_num"  min="1" max="100"><br>
 
           <label>User color:</label>
           <input type="color" id="un_color"name="user_color"><br>
         </fieldset>
-        <fieldset>
-        <label for="job">Job Role:</label>
-        <select id="job" name="user_job">
-          <optgroup label="Web">
-            <option value="frontend_developer">Front-End Developer</option>
-            <option value="php_developor">PHP Developer</option>
-            <option value="python_developer">Python Developer</option>
-            <option value="rails_developer"> Rails Developer</option>
-            <option value="web_designer">Web Designer</option>
-            <option value="WordPress_developer">WordPress Developer</option>
-          </optgroup>
-          <optgroup label="Mobile">
-            <option value="Android_developer">Androild Developer</option>
-            <option value="iOS_developer">iOS Developer</option>
-            <option value="mobile_designer">Mobile Designer</option>
-          </optgroup>
-          <optgroup label="Business">
-            <option value="business_owner">Business Owner</option>
-            <option value="freelancer">Freelancer</option>
-          </optgroup>
-          <optgroup label="Other">
-            <option value="secretary">Secretary</option>
-            <option value="maintenance">Maintenance</option>
-          </optgroup>
-        </select>
-        
-          <label>Interests:</label>
-          <input type="checkbox" id="development" value="interest_development" name="user_interest"><label class="light" for="development">Development</label><br>
-            <input type="checkbox" id="design" value="interest_design" name="user_interest"><label class="light" for="design">Design</label><br>
-          <input type="checkbox" id="business" value="interest_business" name="user_interest"><label class="light" for="business">Business</label>
-          
-          <label for="url">URL:</label>
-          <input type="url" id="site" name="user_site">
 
-        </fieldset>
         <button type="submit">Sign Up</button>
       </form>
       
