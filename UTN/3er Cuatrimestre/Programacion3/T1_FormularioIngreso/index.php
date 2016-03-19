f8bfa26c29f3781587f4998a8f0e9c7f255ce541
Diana <diana.man.91@gmail.com>
2016-03-19 14:01:07 -0300

    controles opcionales: text, color, url

--- UTN/3er Cuatrimestre/Programacion3/T1_FormularioIngreso/index.php
+++ UTN/3er Cuatrimestre/Programacion3/T1_FormularioIngreso/index.php
@@ -39,6 +39,9 @@
 
           <label>Favorite number:</label>
           <input type="number" id="fav_num"name="user_num"  min="1" max="100"><br>
+
+          <label>User color:</label>
+          <input type="color" id="un_color"name="user_color"><br>
         </fieldset>
         <fieldset>
         <label for="job">Job Role:</label>
@@ -70,7 +73,10 @@
           <input type="checkbox" id="development" value="interest_development" name="user_interest"><label class="light" for="development">Development</label><br>
             <input type="checkbox" id="design" value="interest_design" name="user_interest"><label class="light" for="design">Design</label><br>
           <input type="checkbox" id="business" value="interest_business" name="user_interest"><label class="light" for="business">Business</label>
-        
+          
+          <label for="url">URL:</label>
+          <input type="url" id="site" name="user_site">
+
         </fieldset>
         <button type="submit">Sign Up</button>
       </form>
