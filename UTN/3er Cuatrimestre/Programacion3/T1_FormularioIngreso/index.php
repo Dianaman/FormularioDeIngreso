5e180b019599797412cdb8f2b703da839f97317a
Diana <diana.man.91@gmail.com>
2016-03-19 14:20:22 -0300

    idioma español

--- UTN/3er Cuatrimestre/Programacion3/T1_FormularioIngreso/index.php
+++ UTN/3er Cuatrimestre/Programacion3/T1_FormularioIngreso/index.php
@@ -10,62 +10,65 @@
 
       <form action="index.html" method="post">
       
-        <h1>Sign Up</h1>
+        <h1>Registro</h1>
         
         <fieldset>
-          <legend><span class="number">1</span>Your basic info</legend>
-          <label for="name">Name:</label>
+          <legend><span class="number">1</span>Información básica</legend>
+          <label for="name">Nombre de usuario:</label>
           <input type="text" id="name" name="user_name">
           
-          <label for="mail">Email:</label>
+          <label for="mail">Correo electrónico:</label>
           <input type="email" id="mail" name="user_email">
           
-          <label for="password">Password:</label>
+          <label for="password">Contraseña:</label>
           <input type="password" id="password" name="user_password">
           
-          <label>Birthday:</label>
+          <label for="password">Vuelva a ingresar la contraseña:</label>
+          <input type="password" id="password2" name="user_password2">
+
+          <label>Fecha de nacimiento:</label>
           <input type="date" id="birth" name="user_age"><br>
 
-          <label>Gender:</label>
-          <input type="radio" id="male" value="male" name="user_gender">Male<br>
-          <input type="radio" id="female" value="female" name="user_gender">female<br>
+          <label>Sexo:</label>
+          <input type="radio" id="male" value="male" name="user_gender">Masculino<br>
+          <input type="radio" id="female" value="female" name="user_gender">Femenino<br>
           
         </fieldset>
         
         <fieldset>
-          <legend><span class="number">2</span>Your job info</legend>
-          <label for="bio">Biography:</label>
+          <legend><span class="number">2</span>Información laboral</legend>
+          <label for="bio">Biografía:</label>
           <textarea id="bio" name="user_bio"></textarea>
-          
-          <label for="job">Job Role:</label>
+
+          <label for="job">Ocupación:</label>
           <select id="job" name="user_job">
             <optgroup label="Web">
-              <option value="frontend_developer">Front-End Developer</option>
-              <option value="php_developor">PHP Developer</option>
-              <option value="python_developer">Python Developer</option>
-              <option value="rails_developer"> Rails Developer</option>
-              <option value="web_designer">Web Designer</option>
-              <option value="WordPress_developer">WordPress Developer</option>
+              <option value="frontend_developer">Desarrollados Front-End</option>
+              <option value="php_developor">Desarrollador PHP</option>
+              <option value="python_developer">Desarrollador Python</option>
+              <option value="rails_developer">Desarrollador Rails</option>
+              <option value="web_designer">Diseñador Web</option>
+              <option value="WordPress_developer">Desarrollador WordPress</option>
             </optgroup>
             <optgroup label="Mobile">
-              <option value="Android_developer">Androild Developer</option>
-              <option value="iOS_developer">iOS Developer</option>
-              <option value="mobile_designer">Mobile Designer</option>
+              <option value="Android_developer">Desarrollador Androild</option>
+              <option value="iOS_developer">Desarrollador iOS</option>
+              <option value="mobile_designer">Diseñador Mobile</option>
             </optgroup>
-            <optgroup label="Business">
-              <option value="business_owner">Business Owner</option>
-              <option value="freelancer">Freelancer</option>
+            <optgroup label="Empresarial">
+              <option value="business_owner">Empresario</option>
+              <option value="freelancer">Trabajador Freelance</option>
             </optgroup>
-            <optgroup label="Other">
-              <option value="secretary">Secretary</option>
-              <option value="maintenance">Maintenance</option>
+            <optgroup label="Otros">
+              <option value="secretary">Secretario</option>
+              <option value="maintenance">Mantenimiento</option>
             </optgroup>
           </select>
           
-          <label>Interests:</label>
-          <input type="checkbox" id="development" value="interest_development" name="user_interest"><label class="light" for="development">Development</label><br>
-            <input type="checkbox" id="design" value="interest_design" name="user_interest"><label class="light" for="design">Design</label><br>
-          <input type="checkbox" id="business" value="interest_business" name="user_interest"><label class="light" for="business">Business</label><br>
+          <label>Intereses:</label>
+          <input type="checkbox" id="development" value="interest_development" name="user_interest"><label class="light" for="development">Desarrollo</label><br>
+            <input type="checkbox" id="design" value="interest_design" name="user_interest"><label class="light" for="design">Diseño</label><br>
+          <input type="checkbox" id="business" value="interest_business" name="user_interest"><label class="light" for="business">Empresarial</label><br>
           <br>
           
           <label for="url">URL:</label>
@@ -74,14 +77,14 @@
         </fieldset>
         <fieldset>
           <legend><span class="number">3</span>Extras</legend>
-          <label>Favorite number:</label>
+          <label>Número favorito:</label>
           <input type="number" id="fav_num"name="user_num"  min="1" max="100"><br>
 
-          <label>User color:</label>
+          <label>Color de usuario:</label>
           <input type="color" id="un_color"name="user_color"><br>
         </fieldset>
 
-        <button type="submit">Sign Up</button>
+        <button type="submit">Registrame</button>
       </form>
       
     </body>
